9d220950a6c678c337b61ffd4f9674dd82489f29
Coxeyyy <artemcxy@gmail.com>
2023-12-18 20:03:27 +0300

    change giveCredit, repaymentCredit

--- src/main/java/com/coxey/app/system/BankBackSystem.java
+++ src/main/java/com/coxey/app/system/BankBackSystem.java
@@ -4,22 +4,20 @@ import com.coxey.app.request.Request;
 
 public class BankBackSystem {
     private long balanceBank = 10_000;
-    private Request request;
 
     public void getRequestFromHandler(Request request) {
-        this.request = request;
         var typeRequest = request.getRequestType();
         switch(typeRequest) {
             case GET_CREDIT:
-                giveCredit(request.getAmount());
+                giveCredit(request.getAmount(), request);
                 break;
             case REPAYMENT_CREDIT:
-                repaymentCredit(request.getAmount());
+                repaymentCredit(request.getAmount(), request);
                 break;
         }
     }
 
-    public synchronized void giveCredit(long amount) {
+    public synchronized void giveCredit(long amount, Request request) {
         if(balanceBank - amount < 0) {
             System.out.printf("Бэк система: ЗАЯВКА ОТКЛОНЕНА от %s. Сумма кредита %d больше, чем остаток" +
                             " денег банка: %d\r\n", request.getName(), amount, balanceBank);
@@ -30,7 +28,7 @@ public class BankBackSystem {
                 request.getName(), amount, balanceBank);
     }
 
-    public synchronized void repaymentCredit(long amount) {
+    public synchronized void repaymentCredit(long amount, Request request) {
         balanceBank += amount;
         System.out.printf("Бэк система: Банковский счет от %s ПОПОЛНЕН, balance = %d\r\n",
                 request.getName(), balanceBank);
